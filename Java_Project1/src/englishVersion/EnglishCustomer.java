4133ed67976acb33508ec9fc9969ece7e8ae2c06
hyun Geun Gwak <48711195+rgr3287@users.noreply.github.com>
2021-07-19 09:45:48 +0900

    고객의 입력값을 받아 고객들을 분류 및 원하는 데이터값에 따른 오름차순 정렬해서 보기

---
+++ Java_Project1/src/englishVersion/EnglishCustomer.java
@@ -0,0 +1,81 @@
+package englishVersion;
+
+import java.util.Comparator;
+
+public class EnglishCustomer implements Comparable<EnglishCustomer>{
+
+	int age;
+	int out; // 분류 기준
+	String gender;
+	String location;
+	String id;
+	
+	EnglishCustomer(){ //this(0, null, null, null);
+		
+	}
+	
+	EnglishCustomer(int age, String id, String gender, String location, int out){
+		this.age = age;
+		this.gender = gender;
+		this.location = location;
+		this.out = out;
+		this.id = id;
+	}
+	
+	public String toString () { 
+		return 
+		    "▶ 【ID】 " + id + " 【Online use Time】 "+ out + " 【Age】 " + age  
+		+ "\n  【Genger】 "+ gender + " 【Location】 "+ location;
+	}
+	
+	@Override
+	public int compareTo(EnglishCustomer c) {
+		// TODO Auto-generated method stub
+		if (out > c.out) return -1; 
+		else if (this.out == c.out) return id.compareTo(c.id);
+		else return 1;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public int getOut() {
+		return out;
+	}
+	public void setOut(int out) {
+		this.out = out;
+	}
+
+
+
+	
+
+	
+
+
+}
