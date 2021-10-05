54ddfd216341d8149f5863794525319796b91983
곽현근 <48711195+rgr3287@users.noreply.github.com>
2021-10-05 23:36:23 +0900

    네이버 로그인

---
+++ naverlogin/src/main/java/com/springboot/NaverloginApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverloginApplication.class, args);
+	}
+
+}
