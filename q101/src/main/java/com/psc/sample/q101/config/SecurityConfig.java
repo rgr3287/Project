fb99a471575970111db4083e93a8f31adc34c57f
곽현근 <48711195+rgr3287@users.noreply.github.com>
2021-10-12 02:45:27 +0900

    spring boot Oauth2를 사용한 구글,페이스북,네이버,카카오 로그인

---
+++ q101/src/main/java/com/psc/sample/q101/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.psc.sample.q101.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.psc.sample.q101.service.CustomOAuth2UserService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	CustomOAuth2UserService customOAuth2UserService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/login").permitAll()
+				.antMatchers("/user").hasRole("User")
+				.anyRequest().authenticated()
+				.and()
+				.exceptionHandling().accessDeniedPage("/accessDenied")
+				.and()
+				.logout().logoutUrl("/logout")
+				.logoutSuccessUrl("/").permitAll()
+				.and()
+				.oauth2Login().loginPage("/login")
+				.userInfoEndpoint()
+				.userService(customOAuth2UserService);
+				
+	}
+	
+	
+}
