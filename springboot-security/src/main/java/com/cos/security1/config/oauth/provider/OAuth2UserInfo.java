fa0fab3a5d9485c9a32a4ee1a519f7d932c6a7ed
곽현근 <48711195+rgr3287@users.noreply.github.com>
2021-10-12 02:42:39 +0900

    Delete springboot-security directory

--- springboot-security/src/main/java/com/cos/security1/config/oauth/provider/OAuth2UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.cos.security1.config.oauth.provider;
-
-//OAuth2.0 제공자들 마다 응답해주는 속성값이 달라서 공통으로 만들어준다.
-public interface OAuth2UserInfo {
-	String getProviderId();
-	String getProvider();
-	String getEmail();
-	String getName();
-}
